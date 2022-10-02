cbc4b4092dc4fb332d00e96aeee52912e9290676
Vibhav <vibhavsharmaha@gmail.com>
2022-10-02 04:27:22 -0700

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import { Button } from "react-bootstrap";
+import NavBar from "./Navbar/NavBar";
 function App() {
   return (
     <>
-      {/* Navbar */}
+      <NavBar />
       {/* about */}
       {/* subscription dropdowns which is a clickable button: */}
       {/* duaration of subscription */}
