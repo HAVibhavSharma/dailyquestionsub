cb8630e897d59861719c02c51482e0523d978345
Vibhav <vibhavsharmaha@gmail.com>
2022-10-03 04:25:51 -0700

    merged the changes

--- src/components/Navbar/NavBar.js
+++ src/components/Navbar/NavBar.js
@@ -1,11 +1,11 @@
 import React from "react";
-import {Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { useSelector, useDispatch } from "react-redux";
 import { counterActions } from "../../store/reducer";
-
+import AuthForm from "../Auth/AuthForm";
 function NavBar() {
   const counter = useSelector((state) => state.counter);
   const dispatch = useDispatch();
@@ -22,7 +22,7 @@ function NavBar() {
             <Nav className="me-auto">
               <Nav.Link href="#home">Home</Nav.Link>
               <Nav.Link href="#link">About</Nav.Link>
-              <AuthForm Login />
+              {/* <AuthForm Login /> */}
               <Nav.Link href="#link">Subscription</Nav.Link>
               <Nav.Link href="#link">About</Nav.Link>
               <Nav.Link href="#link">Contact Us</Nav.Link>
